ddb6739d411e6715551f308bec285c824230e60f
hitenvora <120244896+hitenvora@users.noreply.github.com>
2023-01-06 16:37:40 +0530

    Add files via upload

---
+++ assisment 6jan2023/table_name.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 06, 2023 at 12:06 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.0.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `employee`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table_name`
+--
+
+CREATE TABLE `table_name` (
+  `email` varchar(255) DEFAULT NULL,
+  `FIRST_name` varchar(255) DEFAULT NULL,
+  `LAST_name` varchar(255) DEFAULT NULL,
+  `salary` float DEFAULT NULL,
+  `joining_date` varchar(255) DEFAULT NULL,
+  `department` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
